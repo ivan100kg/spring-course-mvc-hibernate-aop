dded226c4a383a927a814f3dab3a2780e80a7bbd
Ivan B <ivar110kg@gmail.com>
2022-07-01 11:23:37 +0400

    Add EmployeeServiceImpl.java

---
+++ src/main/java/io/github/ivan100kg/spring/mvc_hibernate_aop/service/EmployeeServiceImpl.java
@@ -0,0 +1,22 @@
+package io.github.ivan100kg.spring.mvc_hibernate_aop.service;
+
+import io.github.ivan100kg.spring.mvc_hibernate_aop.dao.EmployeeDAO;
+import io.github.ivan100kg.spring.mvc_hibernate_aop.entity.Employee;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+public class EmployeeServiceImpl implements EmployeeService{
+
+    @Autowired
+    EmployeeDAO employeeDAO;
+
+    @Override
+    @Transactional  // авто открытие/закрытие транзакций
+    public List<Employee> getAllEmployees() {
+        return employeeDAO.getAllEmployees();
+    }
+}
