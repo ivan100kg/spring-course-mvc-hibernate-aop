d6a1e9a42aecacb9f984af019dd69193e85b9dbc
Ivan B <ivar110kg@gmail.com>
2022-07-01 12:43:34 +0400

    Add new methods to MyController

--- src/main/java/io/github/ivan100kg/spring/mvc_hibernate_aop/controller/MyController.java
+++ src/main/java/io/github/ivan100kg/spring/mvc_hibernate_aop/controller/MyController.java
@@ -6,6 +6,7 @@ import io.github.ivan100kg.spring.mvc_hibernate_aop.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -21,5 +22,18 @@ public class MyController {
         List<Employee> allEmployees = employeeService.getAllEmployees();
         model.addAttribute("allEmps", allEmployees);
         return "all-employees";
+    }
+
+    @RequestMapping("/addNewEmployee")
+    public String addNewEmployee(Model model){
+        Employee employee = new Employee();
+        model.addAttribute("employee", employee);
+        return "employee-info";
+    }
+
+    @RequestMapping("/saveEmployee")
+    public String saveEmployee(@ModelAttribute("employee") Employee employee) {
+        employeeService.saveEmployee(employee);
+        return "redirect:/";
     }
 }
